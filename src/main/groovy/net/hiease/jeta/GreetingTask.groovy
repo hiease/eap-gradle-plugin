6e59edb00657a47f5d5eaa9f9026659ce98bdb20
qihy <qihaiyan@haiercash.com>
2017-07-27 16:16:37 +0800

    add groovy template

--- src/main/groovy/net/hiease/jeta/GreetingTask.groovy
+++ src/main/groovy/net/hiease/jeta/GreetingTask.groovy
@@ -2,13 +2,29 @@ package net.hiease.jeta;
 
 import org.gradle.api.DefaultTask;
 import org.gradle.api.tasks.TaskAction;
+import groovy.text.Template 
+import groovy.text.SimpleTemplateEngine 
+import java.io.File
+
+class Person{ 
+String age 
+String fname 
+String lname 
+String toString(){ 
+ return "Age: " + age + " First Name: " + fname + " Last Name: " + lname 
+} 
+}
 
 class GreetingTask extends DefaultTask {
     String message = 'hello from GreetingTask'
-    final String template = "Dear ${message}";
 
     @TaskAction
     def greet() {
-        println "Dear ${message}"
+    def     pers1 = new Person(age:12, fname:"Sam", lname:"Covery") 
+   def  fle = new File("person_report.tmpl") 
+   def binding = ["p":pers1] 
+   def engine = new SimpleTemplateEngine() 
+   def template = engine.createTemplate(fle).make(binding) 
+   println template.toString() 
     }
 }
